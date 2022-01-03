8029aaffdf8d48f56736ee94fc48e8245ed26ca8
Стоянов Иван <bahr.bahrovich@yandex.ru>
2022-01-03 15:42:52 +0300

    fixes

--- models.py
+++ models.py
@@ -1,8 +1,15 @@
 from pony.orm import *
+import os
+import re
+from urllib.parse import urlparse
 
 db = Database()
 
-db.bind(provider='postgres', user='postgres', password='Tiraspol_2003', host='localhost', database='domain4ik')
+# db.bind(provider='postgres', user='postgres', password='', host='localhost', database='')
+
+db_url = urlparse(os.environ['DATABASE_URL'])
+args = re.split('[:@]', db_url.netloc)
+db.bind(provider=db_url.scheme, user=args[0], password=args[1], host=args[2], port=args[3], database=db_url.path[1:])
 
 
 class User(db.Entity):
